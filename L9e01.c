85cc2da1d074c1cbe7c3d8385220c5cf1702b6c9
Gustavo Henrique Oliveira Villar <gustavohenriquevillar@gmail.com>
2017-11-08 12:56:11 -0200

    Add files via upload

--- L9e01.c
+++ L9e01.c
@@ -2,28 +2,28 @@
 Turma 01
 Dev-Cpp
 25/10/17
-Lista 9 - Todos exercícios
+Lista 9 - Todos exercï¿½cios
 
-Parte 1 - Algoritmos de Ordenação e busca
-Nesta primeira parte você deve ter implementado as seguintes funções, todas elas referentes a vetores com valores inteiros.
-1. uma função que implemente o algoritmo da bolha;
-2. uma função que implemente o algoritmo da seleção;
-3. uma função que implemente o algoritmo de inserção;
-4. uma função que implemente o algoritmo de Shell Sort;
-5. uma função que implemente o algoritmo mergesort;//a ser implementado hoje
-6. uma funçao que implemente o algoritmo quicksort;//a ser implementada hoje
-7. uma função que implemente busca sequencial;
-8. uma função que implemente busca binária;
-9. as funções GerarVetorAleatorio, tempo e verifica_ordem;
+Parte 1 - Algoritmos de Ordenaï¿½ï¿½o e busca
+Nesta primeira parte vocï¿½ deve ter implementado as seguintes funï¿½ï¿½es, todas elas referentes a vetores com valores inteiros.
+1. uma funï¿½ï¿½o que implemente o algoritmo da bolha;
+2. uma funï¿½ï¿½o que implemente o algoritmo da seleï¿½ï¿½o;
+3. uma funï¿½ï¿½o que implemente o algoritmo de inserï¿½ï¿½o;
+4. uma funï¿½ï¿½o que implemente o algoritmo de Shell Sort;
+5. uma funï¿½ï¿½o que implemente o algoritmo mergesort;//a ser implementado hoje
+6. uma funï¿½ao que implemente o algoritmo quicksort;//a ser implementada hoje
+7. uma funï¿½ï¿½o que implemente busca sequencial;
+8. uma funï¿½ï¿½o que implemente busca binï¿½ria;
+9. as funï¿½ï¿½es GerarVetorAleatorio, tempo e verifica_ordem;
 
-Parte 2 - Programa principal e uma função para medir o tempo de processamento 
-Você deve implementar um programa principal, que para vários tamanhos de vetores, faça um teste do funcionamento de suas funções. O usuário fornecerá o tempo máximo que seu programa deverá
-executar. Nos testes você deve usar a função verifica_ordem para verificar se os algoritmos estão ordenando corretamente e também comparar os resultados das buscas binárias e sequencial.
+Parte 2 - Programa principal e uma funï¿½ï¿½o para medir o tempo de processamento
+Vocï¿½ deve implementar um programa principal, que para vï¿½rios tamanhos de vetores, faï¿½a um teste do funcionamento de suas funï¿½ï¿½es. O usuï¿½rio fornecerï¿½ o tempo mï¿½ximo que seu programa deverï¿½
+executar. Nos testes vocï¿½ deve usar a funï¿½ï¿½o verifica_ordem para verificar se os algoritmos estï¿½o ordenando corretamente e tambï¿½m comparar os resultados das buscas binï¿½rias e sequencial.
 
-Lembre-se que, embora a busca binária seja muito mais rápida do que a sequencial, ela requer que os elementos do vetor estejam ordenados. A ordenação requer um esforço computacional
-adicional. Quando será que vale a pena pagar esse preço adicional? Uma possível forma para dar uma resposta a essa questão é considerar o número de buscas que serão feitas.
-Neste exercício vamos determinar, na prática, para vários valores de n, quando começa a valer a pena a ordenação de um vetor com n inteiros para permitir a busca binária.
-O programa main, e a função tempo, pode ter o seguinte esqueleto:
+Lembre-se que, embora a busca binï¿½ria seja muito mais rï¿½pida do que a sequencial, ela requer que os elementos do vetor estejam ordenados. A ordenaï¿½ï¿½o requer um esforï¿½o computacional
+adicional. Quando serï¿½ que vale a pena pagar esse preï¿½o adicional? Uma possï¿½vel forma para dar uma resposta a essa questï¿½o ï¿½ considerar o nï¿½mero de buscas que serï¿½o feitas.
+Neste exercï¿½cio vamos determinar, na prï¿½tica, para vï¿½rios valores de n, quando comeï¿½a a valer a pena a ordenaï¿½ï¿½o de um vetor com n inteiros para permitir a busca binï¿½ria.
+O programa main, e a funï¿½ï¿½o tempo, pode ter o seguinte esqueleto:
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -47,7 +47,7 @@ int main(){
 	printf("\n Todos os metodos<7>");
 	printf("\n");
 	scanf("%d",&opcao);
-	//gera um numero aleatorio para ser procurado no vetor 
+	//gera um numero aleatorio para ser procurado no vetor
 	numero = (double) INT_MAX*rand() / (RAND_MAX + 1.0);
 	printf("\n Numero a ser procurado: %.2f\n", numero);
 	switch (opcao){
@@ -56,16 +56,16 @@ int main(){
 				.
 				.
 				.
-				
-				
-O seu programa deve permitir as opções de escolha dos altoritmos descritos acima, deverá testar valores de n bem grandes (por exemplo: a partir de 2000, com acrescimento de 2000 a cada passo).
-Para cada valor de n, o seu programa deverá informar, para cada método especificado, qual o valor mínimo do numero de buscar a partir do qual vale a pena fazer a ordenação e busca binária em vez
-de busca sequencial sem ordenação.
-O processo para quando for detectado que seu programa já usou um tempo maior do que a duração especificada peo usuário.
-Escreva o programa principal de forma que o usuário possa escolher um determinado método de ordenação ou poderá decidir executar o programa utilizando todos os métodos de ordenação de uma única vez.
-Para tamanhos de vetores não muito grandes, é comum que os tempos de execução sejam zero, pois como os processos são muito rápidos, a medida de tempo não consegue capturar essa passagem rápida do tempo.
-Assim, outras formas de mostrar a eficiência de um método em relação a outros é a comparação do número de trocas efetudas e, também a quantidade de iterações efetuadas pelos métodos para ordenar os vetores.
-Implemente contadores para essas formas, de maneira que o sue programa consiga dar ao usuário um panorama de eficácia dos métodos de ordenação.
+
+
+O seu programa deve permitir as opï¿½ï¿½es de escolha dos altoritmos descritos acima, deverï¿½ testar valores de n bem grandes (por exemplo: a partir de 2000, com acrescimento de 2000 a cada passo).
+Para cada valor de n, o seu programa deverï¿½ informar, para cada mï¿½todo especificado, qual o valor mï¿½nimo do numero de buscar a partir do qual vale a pena fazer a ordenaï¿½ï¿½o e busca binï¿½ria em vez
+de busca sequencial sem ordenaï¿½ï¿½o.
+O processo para quando for detectado que seu programa jï¿½ usou um tempo maior do que a duraï¿½ï¿½o especificada peo usuï¿½rio.
+Escreva o programa principal de forma que o usuï¿½rio possa escolher um determinado mï¿½todo de ordenaï¿½ï¿½o ou poderï¿½ decidir executar o programa utilizando todos os mï¿½todos de ordenaï¿½ï¿½o de uma ï¿½nica vez.
+Para tamanhos de vetores nï¿½o muito grandes, ï¿½ comum que os tempos de execuï¿½ï¿½o sejam zero, pois como os processos sï¿½o muito rï¿½pidos, a medida de tempo nï¿½o consegue capturar essa passagem rï¿½pida do tempo.
+Assim, outras formas de mostrar a eficiï¿½ncia de um mï¿½todo em relaï¿½ï¿½o a outros ï¿½ a comparaï¿½ï¿½o do nï¿½mero de trocas efetudas e, tambï¿½m a quantidade de iteraï¿½ï¿½es efetuadas pelos mï¿½todos para ordenar os vetores.
+Implemente contadores para essas formas, de maneira que o sue programa consiga dar ao usuï¿½rio um panorama de eficï¿½cia dos mï¿½todos de ordenaï¿½ï¿½o.
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -108,8 +108,8 @@ Implemente contadores para essas formas, de maneira que o sue programa consiga d
             }
             return trocas;
         }
-        
-//Metodo de inserção 
+
+//Metodo de inserÃ§ao
         int inserctionSort(float v[], int n) {
             int i, j, trocas=0;
             float x;
@@ -125,7 +125,7 @@ Implemente contadores para essas formas, de maneira que o sue programa consiga d
             }
             return trocas;
         }
-        
+
 //Metodo de Shell Sort (Knut)
   int knutt(int i){
     if(i==1)
@@ -164,23 +164,35 @@ Implemente contadores para essas formas, de maneira que o sue programa consiga d
     }
     return trocas;
   }
-  
+
 //Algoritmo mergesort
-	void intercala (int V[], int p, int q, int r){
-		int i,j,k,*w;
-		w=(int*)malloc((r-p)*sizeof(int));
-		i=p;
-		k=0;
-		j=q;
-		while(i<q && j<r)
-		if(V[i]<=V[j])
-		w[k++]=V[i++];
-		else w[k++]=V[j++];
-		while(i<q) w[k++]=V[i++];
-		while(j<r) w[k++]=V[j++];
-		for(i=0;i<k;i++) V[p+i]=w[i];
-		free(w); 
-	}
+void intercala (int v[], int p, int q, int r) {
+    int i, j, k, *w;
+    w = (int*) malloc ((r-p)*sizeof(int));
+    i=p;
+    k=0;
+    j=q;
+    while(i<q && j<r)
+        if(v[i]<=v[j])
+            w[k++]=v[i++];
+        else
+            w[k++] = v[j++];
+    while(i<q) w[k++] = v[i++];
+    while(j<r) w[k++] = v[j++];
+    for(i=0; i<k; i++) v[p+i] = w[i];
+    free(w);
+}
+
+void mergeSort (int v[], int p, int r) {
+    if(p<r-1) {
+        int q = (r+p) / 2;
+        mergeSort(v,p,q);
+        mergeSort(v,q,r);
+        intercala(v,p,q,r);
+    }
+}
+
+
 
 //Algoritmo Quicksort
 	void Quicksort(int V[],int e, int d){
@@ -200,7 +212,7 @@ Implemente contadores para essas formas, de maneira que o sue programa consiga d
 		if(e<j) Quicksort(V,e,j);
 		if(i<d) Quicksort(V,i,d);
 	}
-	
+
 //Busca Sequencial
     int buscaSequencial (float v[], int n, float x) {
         int m;
@@ -209,7 +221,7 @@ Implemente contadores para essas formas, de maneira que o sue programa consiga d
         else if(v[m]==x) return m;
         else return -1;
     }
-    
+
 //Busca Binaria
     int buscaBinaria (float *v, int n, float x) { // Vetor deve ser ordenado
         int m, e, d;
@@ -224,8 +236,8 @@ Implemente contadores para essas formas, de maneira que o sue programa consiga d
         }
         return -1;
     }
-    
-//Gerar vetor aleatório
+
+//Gerar vetor aleatï¿½rio
 void vetor_aleatorio(float vetor[1000], int tamanho){
   int i;
   for(i=0;i<tamanho;i++)
@@ -234,7 +246,7 @@ void vetor_aleatorio(float vetor[1000], int tamanho){
   }
 }
 
-//Verificar Ordem 
+//Verificar Ordem
     int verificaOrdem (float *v, int n) {
         int i, x=1;
         n--;
@@ -242,14 +254,193 @@ void vetor_aleatorio(float vetor[1000], int tamanho){
             if(v[i]>v[i+1]) x = 0;
         return x;
     }
-    
-//Tempo 
+
+//Tempo
 double tempo(){
 	return (double) clock()/CLOCKS_PER_SEC;
 }
 
+//Copiar vetor
+void copiarVetor(int v1[], int v2[], int n){
+  int i;
+  for (i=0;i<n;i++)
+      v2[i] = v1[i];
+}
 
 
+//Programa principal
 int main(int argc, char *argv[]) {
+  int i, op, v[100], copia[100];
+  double duracao, ti, tf, total, tbusca, numero;
+  puts("Escolha o algortimo que deseja:\n1-Bolha\n2-Selection\n3-Insertion\n4-Shell Sort\n5-Merge Sort\n6-Quick Sort\n7-Todos anteriores.");
+  scanf("%d",&op);
+  puts("Digite o tempo maximo, em segundos, que o programa deve executar:");
+  scanf("%f",&duracao);
+  total = 0;
+  i=2000;
+  if(op==1)
+  {
+    puts("Bubble Sort");
+    while(total<duracao)
+    {
+      vetor_aleatorio(v,i);
+      ti = tempo();
+      buscaSequencial(v,i,numero);
+      tf = tempo();
+      tbusca = tf-ti;
+      ti = tempo();
+      bubbleSort(v,i);
+      tf = tempo();
+      total += (tf-ti) + tbusca;
+      printf("%d numeros\n",i);
+      printf("Ordenar %lfms [%d]\n",tf-ti,verificaOrdem(v,i));
+      printf("Busca Sequencial %lfms\n",tbusca);
+      ti=tempo();
+      buscaBinaria(v,i,numero);
+      tf=tempo();
+      tbusca=tf-ti;
+      total = total + tbusca;
+      printf("Busca Binaria %lfms\n",tbusca);
+      i=i+2000;
+    }
+  }
+  else if(op==2)
+  {
+    puts("Selection Sort");
+    while(total<duracao)
+    {
+      vetor_aleatorio(v,i);
+      ti = tempo();
+      buscaSequencial(v,i,numero);
+      tf = tempo();
+      tbusca = tf-ti;
+      ti = tempo();
+      selectionSort(v,i);
+      tf = tempo();
+      total += (tf-ti) + tbusca;
+      printf("%d numeros\n",i);
+      printf("Ordenar %lfms [%d]\n",tf-ti,verificaOrdem(v,i));
+      printf("Busca Sequencial %lfms\n",tbusca);
+      ti=tempo();
+      buscaBinaria(v,i,numero);
+      tf=tempo();
+      tbusca=tf-ti;
+      total = total + tbusca;
+      printf("Busca Binaria %lfms\n",tbusca);
+      i=i+2000;
+    }
+  }
+  else if(op==3)
+  {
+    puts("Inserction Sort");
+    while(total<duracao)
+    {
+      vetor_aleatorio(v,i);
+      ti = tempo();
+      buscaSequencial(v,i,numero);
+      tf = tempo();
+      tbusca = tf-ti;
+      ti = tempo();
+      inserctionSort(v,i);
+      tf = tempo();
+      total += (tf-ti) + tbusca;
+      printf("%d numeros\n",i);
+      printf("Ordenar %lfms [%d]\n",tf-ti,verificaOrdem(v,i));
+      printf("Busca Sequencial %lfms\n",tbusca);
+      ti=tempo();
+      buscaBinaria(v,i,numero);
+      tf=tempo();
+      tbusca=tf-ti;
+      total = total + tbusca;
+      printf("Busca Binaria %lfms\n",tbusca);
+      i=i+2000;
+    }
+  }
+  else if(op==4)
+  {
+    puts("Shell Sort");
+    while(total<duracao)
+    {
+      vetor_aleatorio(v,i);
+      ti = tempo();
+      buscaSequencial(v,i,numero);
+      tf = tempo();
+      tbusca = tf-ti;
+      ti = tempo();
+      ShellSortKnutt(v,i);
+      tf = tempo();
+      total += (tf-ti) + tbusca;
+      printf("%d numeros\n",i);
+      printf("Ordenar %lfms [%d]\n",tf-ti,verificaOrdem(v,i));
+      printf("Busca Sequencial %lfms\n",tbusca);
+      ti=tempo();
+      buscaBinaria(v,i,numero);
+      tf=tempo();
+      tbusca=tf-ti;
+      total = total + tbusca;
+      printf("Busca Binaria %lfms\n",tbusca);
+      i=i+2000;
+    }
+  }
+  else if(op==5)
+  {
+    puts("Merge Sort");
+    while(total<duracao)
+    {
+      vetor_aleatorio(v,i);
+      ti = tempo();
+      buscaSequencial(v,i,numero);
+      tf = tempo();
+      tbusca = tf-ti;
+      ti = tempo();
+      mergeSort(v,0,i);
+      tf = tempo();
+      total += (tf-ti) + tbusca;
+      printf("%d numeros\n",i);
+      printf("Ordenar %lfms [%d]\n",tf-ti,verificaOrdem(v,i));
+      printf("Busca Sequencial %lfms\n",tbusca);
+      ti=tempo();
+      buscaBinaria(v,i,numero);
+      tf=tempo();
+      tbusca=tf-ti;
+      total = total + tbusca;
+      printf("Busca Binaria %lfms\n",tbusca);
+      i=i+2000;
+    }
+  }
+  else if(op==6)
+  {
+    puts("Quick Sort");
+    while(total<duracao)
+    {
+      vetor_aleatorio(v,i);
+      ti = tempo();
+      buscaSequencial(v,i,numero);
+      tf = tempo();
+      tbusca = tf-ti;
+      ti = tempo();
+      quickSort(v,0,i);
+      tf = tempo();
+      total += (tf-ti) + tbusca;
+      printf("%d numeros\n",i);
+      printf("Ordenar %lfms [%d]\n",tf-ti,verificaOrdem(v,i));
+      printf("Busca Sequencial %lfms\n",tbusca);
+      ti=tempo();
+      buscaBinaria(v,i,numero);
+      tf=tempo();
+      tbusca=tf-ti;
+      total = total + tbusca;
+      printf("Busca Binaria %lfms\n",tbusca);
+      i=i+2000;
+    }
+  }
+/*  else if(op==7)
+  {
+
+  }*/
+  else
+  {
+    puts("Invalid option.");
+  }
 	return 0;
 }
